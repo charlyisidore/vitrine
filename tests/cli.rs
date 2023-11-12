f1249ae986e5ad238e9f466d344da8b4d3efc275
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2023-11-12 16:53:33 +0800

    More integration tests

--- tests/cli.rs
+++ tests/cli.rs
@@ -31,6 +31,146 @@ fn fail_config_file_not_found() -> Result<(), Box<dyn std::error::Error>> {
 }
 
 #[test]
+fn config_default_json() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("vitrine.config.json").write_str(
+        r#"{
+  "input_dir": "content",
+  "output_dir": "public"
+}
+"#,
+    )?;
+
+    dir.child("content/index.md").write_str("Home")?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("vitrine.config.json"));
+
+    dir.child("public/index.html")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("Home"));
+
+    Ok(())
+}
+
+#[test]
+fn config_default_lua() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("vitrine.config.lua").write_str(
+        r#"-- Config
+return {
+    input_dir = "content",
+    output_dir = "public",
+}
+"#,
+    )?;
+
+    dir.child("content/index.md").write_str("Home")?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("vitrine.config.lua"));
+
+    dir.child("public/index.html")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("Home"));
+
+    Ok(())
+}
+
+#[test]
+fn config_default_rhai() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("vitrine.config.rhai").write_str(
+        r#"// Config
+#{
+    input_dir: "content",
+    output_dir: "public",
+}
+"#,
+    )?;
+
+    dir.child("content/index.md").write_str("Home")?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("vitrine.config.rhai"));
+
+    dir.child("public/index.html")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("Home"));
+
+    Ok(())
+}
+
+#[test]
+fn config_default_toml() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("vitrine.config.toml").write_str(
+        r#"# Config
+input_dir = "content"
+output_dir = "public"
+"#,
+    )?;
+
+    dir.child("content/index.md").write_str("Home")?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("vitrine.config.toml"));
+
+    dir.child("public/index.html")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("Home"));
+
+    Ok(())
+}
+
+#[test]
+fn config_default_yaml() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("vitrine.config.yaml").write_str(
+        r#"# Config
+input_dir: content
+output_dir: public
+"#,
+    )?;
+
+    dir.child("content/index.md").write_str("Home")?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert()
+        .success()
+        .stdout(predicate::str::contains("vitrine.config.yaml"));
+
+    dir.child("public/index.html")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("Home"));
+
+    Ok(())
+}
+
+#[test]
 fn javascript() -> Result<(), Box<dyn std::error::Error>> {
     let dir = assert_fs::TempDir::new()?;
 
@@ -53,6 +193,52 @@ document.addEventListener('DOMContentLoaded', () => {
         .assert(predicate::str::contains("addEventListener"))
         .assert(predicate::str::contains("DOMContentLoaded"))
         .assert(predicate::str::contains("Hello, World!"));
+
+    Ok(())
+}
+
+#[test]
+fn markdown() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("index.md").write_str("*Italic*")?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert().success();
+
+    dir.child("_site/index.html")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("<em>Italic</em>"));
+
+    Ok(())
+}
+
+#[test]
+fn scss() -> Result<(), Box<dyn std::error::Error>> {
+    let dir = assert_fs::TempDir::new()?;
+
+    dir.child("style.scss").write_str(
+        r#"// Comment
+body {
+  main {
+    margin: 0;
+  }
+}
+"#,
+    )?;
+
+    let mut cmd = Command::cargo_bin("vitrine")?;
+    cmd.current_dir(&dir);
+
+    cmd.assert().success();
+
+    dir.child("_site/style.css")
+        .assert(predicate::path::is_file())
+        .assert(predicate::str::contains("Comment").not())
+        .assert(predicate::str::contains("body main"))
+        .assert(predicate::str::contains("margin"));
 
     Ok(())
 }
