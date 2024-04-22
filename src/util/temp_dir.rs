5d79188dbc38cabeed27f11f8361837cf8674a4e
Charly Lersteau <645647+charlyisidore@users.noreply.github.com>
2024-04-22 16:12:38 +0200

    Add TempDir utility

---
+++ src/util/temp_dir.rs
@@ -0,0 +1,43 @@
+//! Create temporary directories.
+
+use std::path::{Path, PathBuf};
+
+/// Wraps a temporary directory path.
+///
+/// The directory is removed when this is dropped.
+pub struct TempDir(PathBuf);
+
+impl Drop for TempDir {
+    fn drop(&mut self) {
+        std::fs::remove_dir_all(&self.0).expect("failed to remove temp dir")
+    }
+}
+
+impl TempDir {
+    /// Create a temporary directory.
+    pub fn new() -> Self {
+        let dir = std::env::temp_dir();
+        for _ in 0..10 {
+            let path = dir.join(format!("vitrine_{}", random_number()));
+            if path.exists() {
+                continue;
+            }
+            std::fs::create_dir_all(&path).expect(&format!("failed to create temp dir {:?}", path));
+            return Self(path);
+        }
+        panic!("failed to create temp dir")
+    }
+
+    /// Return the directory path.
+    pub fn path(&self) -> &Path {
+        &self.0
+    }
+}
+
+/// Generate a random number.
+fn random_number() -> u64 {
+    use std::hash::{BuildHasher, Hasher};
+    std::collections::hash_map::RandomState::new()
+        .build_hasher()
+        .finish()
+}
